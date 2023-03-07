86a56607a066ee25c0c309a764ddedfdea6e42e2
maximilianknuerr <39460393+maximilianknuerr@users.noreply.github.com>
2023-03-07 12:52:41 +0100

    added routs to express server

--- server/index.js
+++ server/index.js
@@ -35,3 +35,39 @@ const redisClient = redis.createClient({
     retry_strategy: () => 1000
 })
 const redisPublisher = redisClient.duplicate()
+
+// Express routing
+
+app.get("/", (req, res) => {
+    res.send('Hi')
+})
+
+app.get('/values/all', async (req, res) => {
+    const values = await pgClient.query('SELECT * from values')
+
+    res.send(values.rows)
+})
+
+app.get('/values/current', async (req, res) => {
+    redisClient.hgetall('values', (err, values) => {
+        res.send(values)
+    })
+})
+
+app.post('/values', async (req, res) => {
+    const index = req.body.index
+
+    if(parseInt(index) > 40) {
+        return res.status(422).send('Index to high')
+    }
+
+    redisClient.hset('values', index, 'Nothing yet!')
+    redisPublisher.publish('insert', index)
+    pgClient.query('INDEX INTO values(number) VALUES($1', [index])
+
+    res.send({ working: true })
+})
+
+app.listen(5000, err => {
+    console.log("Listening")
+})
