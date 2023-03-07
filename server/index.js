702465ee8f47b65de77ca0966a5d9601a0841e96
maximilianknuerr <39460393+maximilianknuerr@users.noreply.github.com>
2023-03-07 12:35:15 +0100

    added query to pgClient

--- server/index.js
+++ server/index.js
@@ -22,4 +22,6 @@ const pgClient = new Pool({
 
  pgClient.on('error', () => console.log('Lost PG connection'))
 
- 
+ pgClient
+    .query('CREATE TABLE IF NOT EXISTS values (number INT)')
+    .catch(err => console.log(err))
